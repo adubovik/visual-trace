5af0fb6278a8aca4979a08a86de16a1561fb57eb
Anton Dubovik <gunner.kade@gmail.com>
2014-04-13 23:06:23 +0300

    Added some logic for force-directed graph layout.

---
+++ Data/Graph/Layout.hs
@@ -0,0 +1,71 @@
+{-# language
+   ScopedTypeVariables
+ , TupleSections
+ #-}
+
+module Data.Graph.Layout where
+
+import Graphics.Gloss.Data.Point
+import Data.Graph.Dynamic.Annotated
+import Data.Graph.Layout.Physics
+
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+
+type Graph2D na ea key = AnnGraph (na, Point) ea key
+
+forces :: [Force]
+forces = [ logSpring (2,1)
+         , quadRepel 1
+         ]
+
+moveCoeff :: Float
+moveCoeff = 0.1
+
+deltaEps :: Float
+deltaEps = 0.1
+
+applyForces :: forall na ea key. Ord key => [Force] ->
+               Graph2D na ea key -> (Graph2D na ea key, Float)
+applyForces forces g = (g', avgDelta)
+  where
+    nodePoss :: Map.Map (Node key) Point
+    nodePoss = Map.map snd $ getNodeAnnotations g
+
+    edges :: Set.Set (Edge key)
+    edges = Set.fromList $ concat $
+            [ [ (fr,to)
+              , (to,fr)
+              ]
+            | (fr,to) <- Set.toList (getEdges g)
+            ]
+
+    nodes :: Set.Set (Node key)
+    nodes = Set.fromList $ Map.keys nodePoss
+
+    nodePoss' :: Map.Map (Node key) Point
+    nodePoss' = Map.fromList $
+      [ (fr, moveCoeff `mulVector` force + pos1)
+      | (fr, pos1) <- Map.toList nodePoss
+      , let force = sum $
+                    [ forceVector
+                    | (to, pos2) <- Map.toList nodePoss
+                    , let areConnected = Set.member (fr,to) edges
+                    , let forceVector = sum $ map (\f -> f areConnected pos1 pos2) forces
+                    ]
+      ]
+
+    nodeDeltas :: Map.Map (Node key) Float
+    nodeDeltas = Map.map lengthVector $
+                 Map.unionWith (-) nodePoss nodePoss'
+
+    g' = adjustNodeAnn adjPos g
+      where
+        adjPos node (na,_oldPos) = (na,newPos)
+          where
+            newPos = maybe err id $ Map.lookup node nodePoss'
+            err = error "applyForces: new position for node wasn't found."
+
+    avgDelta = average $ Map.elems nodeDeltas
+      where
+        average ls = sum ls / (fromIntegral (length ls))
---
+++ Data/Graph/Layout/Physics.hs
@@ -0,0 +1,45 @@
+module Data.Graph.Layout.Physics
+ ( logSpring
+ , quadRepel
+ , distance
+ , lengthVector
+ , mulVector
+ , Force
+ ) where
+
+import Graphics.Gloss.Data.Point
+
+type Vector = Point
+type Force = Bool -> Point -> Point -> Vector
+
+distance :: Point -> Point -> Float
+distance (x1,y1) (x2,y2) = sqrt ((x1-x2)**2 + (y1-y2)**2)
+
+mulVector :: Float -> Vector -> Vector
+mulVector m (x,y) = (m*x,m*y)
+
+lengthVector :: Vector -> Float
+lengthVector (x,y) = sqrt (x**2 + y**2)
+
+normalize :: Vector -> Vector
+normalize v = (1 / lengthVector v) `mulVector` v
+
+forceViaDist :: (Float -> Float) -> (Point -> Point -> Vector)
+forceViaDist f = \p1 p2 ->
+  (f $ distance p1 p2) `mulVector` (normalize $ p2 - p1)
+
+logSpring :: (Float, Float) -> Force
+logSpring (c1,c2) = force
+  where
+    f d = c1 * log (d / c2)
+    force _areConnected@True  = forceViaDist f
+    force _areConnected@False = \_ _ -> (0,0)
+
+quadRepel :: Float -> Force
+quadRepel c = force
+  where
+    f d = -(c / d**2)
+    force _areConnected@True  = \_ _ -> (0,0)
+    force _areConnected@False = forceViaDist f
+
+
