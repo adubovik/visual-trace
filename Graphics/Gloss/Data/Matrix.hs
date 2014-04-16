85c26ddd3839e701b8f59b76e4ec9dccf49c701b
Anton Dubovik <gunner.kade@gmail.com>
2014-04-17 01:53:52 +0300

    Further amendments of Matrix API.

--- Graphics/Gloss/Data/Matrix.hs
+++ Graphics/Gloss/Data/Matrix.hs
@@ -3,17 +3,63 @@
 
 module Graphics.Gloss.Data.Matrix
  ( Matrix
- , identityMatrix
+ , identityScale
+ , identityTranslate
+ , applyMatrix
+ , invertMatrix
  ) where
+
+import Graphics.Gloss.Data.Point(Point)
+
+import Data.Monoid
 
 -- Yeah, not really a matrix.
 data Matrix = Matrix
-  { mTranslate :: (Float, Float)
-  , mScale     :: (Float, Float)
+  { mTranslate :: (Float, Float) -- applied 1st
+  , mScale     :: (Float, Float) -- applied 2nd
+  -- TODO: mRotate :: Float
   }
+  deriving (Show)
+
+instance Monoid Matrix where
+  mempty = identityMatrix
+  mappend = composeMatrices
 
 identityMatrix :: Matrix
 identityMatrix = Matrix { mTranslate = (0,0)
                         , mScale     = (1,1)
                         }
 
+identityScale :: (Float, Float) -> Matrix
+identityScale s = Matrix { mTranslate = (0,0)
+                         , mScale     = s
+                         }
+
+identityTranslate :: (Float, Float) -> Matrix
+identityTranslate t = Matrix { mTranslate = t
+                             , mScale     = (1,1)
+                             }
+
+invertScale :: (Float,Float) -> (Float, Float)
+invertScale (x,y) = (1/x,1/y)
+
+invertTranslate :: (Float,Float) -> (Float, Float)
+invertTranslate (x,y) = (-x,-y)
+
+-- Associative operator
+composeMatrices :: Matrix -> Matrix -> Matrix
+composeMatrices m1 m2 = Matrix { mTranslate = mTranslate m2 +
+                                                (invertScale $ mScale m2) *
+                                                mTranslate m1
+                               , mScale = mScale m1 * mScale m2
+                               }
+
+applyMatrix :: Matrix -> Point -> Point
+applyMatrix m = ((mScale m) *) . ((mTranslate m) + )
+
+-- invertMatrix m <> m == mempty
+-- invertMatrix . invertMatrix == id
+invertMatrix :: Matrix -> Matrix
+invertMatrix m = identityTranslate (invertTranslate $ mTranslate m) <>
+                 identityScale     (invertScale     $ mScale     m) <>
+                 mempty
